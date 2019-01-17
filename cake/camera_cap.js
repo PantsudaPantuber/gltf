ac1c1600c5a095e6b25406d52512550a3d5d4f37
PantsudaPantuber <pantsupantsu01>
2019-01-17 17:41:50 +0900

    再実験

--- cake/camera_cap.js
+++ cake/camera_cap.js
@@ -55,10 +55,13 @@ function saveBlob(blob, fileName){
     var event = document.createEvent("MouseEvents");
     event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     var a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+    var b = document.createElement("img");
     // a.href = dataUrl;
     // a.target = '_self';
     // a.download = fileName;
     // a.dispatchEvent(event);
+    b.src = dataUrl;
+    b.setAttribute('id="captest"');
 
     console.log(dataUrl)
     //window.open('data:' + mimeType + ';base64,' + window.Base64.encode(content), '_blank');
